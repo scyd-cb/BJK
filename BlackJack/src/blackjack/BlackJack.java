2fe978291ea599e2de4cbe257424d0cc9d112994
sduong <Duong Steven@StevenDuong-PC>
2014-07-04 18:14:34 -0700

    + add licenceheader.txt > set up class default header

--- BlackJack/src/blackjack/BlackJack.java
+++ BlackJack/src/blackjack/BlackJack.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This program and the accompanying materials
+ * are made available for the coding challenge 
+ * organized by Insight.
  */
 
 package blackjack;
